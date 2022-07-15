42c0ebfe3c2b230a0628463dc546e23a47746c59
ChristianBonino <christianbonino@live.it>
2022-07-15 16:07:37 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using EsercitazioneChry_Login.DB;
+using EsercitazioneChry_Login.DB.Entities;
+using EsercitazioneChry_Login.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsercitazioneChry_Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SignInManager<User> signInManager;
+        private UserManager<User> userManager;
+        private UserDBContext dbContext;
+
+        public HomeController(SignInManager<User> signInManager,
+            UserManager<User> userManager,
+            UserDBContext dbContext)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            try
+            {
+                User user = await userManager.FindByNameAsync(loginModel.UserName);
+                if (user != null)
+                {
+                    var result = await signInManager.PasswordSignInAsync(loginModel.UserName, loginModel.Password, false, lockoutOnFailure: true);
+                    if (result.Succeeded)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return Redirect("Index");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                if (signInManager.IsSignedIn(User))
+                {
+                    await signInManager.SignOutAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return Redirect("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DB/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EsercitazioneChry_Login.DB.Entities
+{
+    public class User : IdentityUser
+    {
+    }
+}
